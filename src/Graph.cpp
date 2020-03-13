7457c858c0705776128aea1454ba29597916ec48
Armando Ortiz <armandopenafiel12@gmail.com>
2020-03-12 22:13:09 -0600

    Google test board

---
+++ src/Graph.cpp
@@ -0,0 +1,53 @@
+#include "../GUI/Include/Extra/Graph.hpp"
+
+namespace graphs
+{
+//Specific graph structures.
+using Vertex = Graph::Vertex;
+
+Graph Grid(int n, int m)
+{
+    Graph G((n + 1)*(m + 1));
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            Vertex v = i*(m + 1) + j;
+            G.add_edge(v, v + 1 );
+            G.add_edge(v, v + m + 1);
+        }
+    }
+
+    for (int j = 0; j < m; ++j)
+    {
+        Vertex v = n*(m + 1) + j;
+        G.add_edge(v, v + 1);
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        Vertex v = i*(m + 1) + m;
+        G.add_edge(v, v + m + 1);
+    }
+
+    return G;
+}
+
+
+Graph Complete(int n)
+{
+    Graph G(n);
+
+    for (Vertex i = 0; i + 1 < n; ++i)
+    {
+        for (Vertex j = i + 1; j < n; ++j)
+        {
+            G.add_edge(i, j);
+        }
+    }
+
+    return G;
+}
+
+}
