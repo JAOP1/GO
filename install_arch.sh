84f7d497ee5a1ddc85e8322ffdd2e3b593ee34e7
Armando Ortiz <armandopenafiel12@gmail.com>
2020-03-17 14:45:24 -0600

    Create installer to arch and manjaro.

---
+++ install_arch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo pacman -S --needed sfml git cmake gtest clang gcc
+rm -rf Graph
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
+git clone https://github.com/JAOP1/GO
+cd GO/
+mkdir build
+cd build
+cmake ..
+make
