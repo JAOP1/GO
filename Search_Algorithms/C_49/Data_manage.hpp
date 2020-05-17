5f08b51260171841538d0d21e79f5c2b3df2df38
Armando Ortiz <armandopenafiel12@gmail.com>
2020-05-17 13:09:47 -0500

    Modificaciones a archivos relacionados con libtorch.

--- Search_Algorithms/C_49/Data_manage.hpp
+++ Search_Algorithms/C_49/Data_manage.hpp
@@ -1,80 +1,108 @@
-#pragma once 
-#include <torch/torch.h>
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <tuple>
+#pragma once
 #include "Include/BoardGame.hpp"
 #include "Include/Extra/Graph.hpp"
 #include "Include/Extra/json_manage.hpp"
 #include "Search_Algorithms/C_49/Net_Class.hpp"
+#include <iostream>
+#include <numeric>
+#include <torch/torch.h>
+#include <tuple>
+#include <vector>
 
+using element = torch::data::Example<torch::Tensor, torch::Tensor>;
 
-using element =  torch::data::Example<torch::Tensor , torch::Tensor>;
+// Estructura para recolectar los datos.
+struct GameDateSet
+    : torch::data::datasets::
+        BatchDataset<GameDateSet, std::vector<element>, std::vector<size_t>>
+{
 
-//Estructura para recolectar los datos.
-struct GameDateSet : torch::data::datasets::BatchDataset<GameDateSet, std::vector<element>, std::vector<size_t> > {
-  
-  explicit GameDateSet(std::vector<element>& sample) : dataset(sample) {}
-  
-  std::vector<element> get_batch(std::vector<size_t> indices) override {
-    
-    std::vector< element >  batch;
+    explicit GameDateSet(std::vector<element>& sample) : dataset(sample) {}
 
-    for (auto i : indices) {
-          batch.push_back(dataset[i]);
+    std::vector<element> get_batch(std::vector<size_t> indices) override
+    {
+
+        std::vector<element> batch;
+
+        for (auto i : indices)
+        {
+            batch.push_back(dataset[i]);
+        }
+
+        return batch;
     }
 
-    return batch;
-  }
-  
-  torch::optional<size_t> size() const override {
-    return dataset.size();
-  }
+    torch::optional<size_t> size() const override { return dataset.size(); }
 
-  std::vector<element> dataset;
+    std::vector<element> dataset;
 };
 
-
-//Mañana realizo esta función.
-template<class game>
-game get_episode(Network_evaluator& Model , std::string slection_mode, BoardGame BG)
+// Solo le falta incluir los parametros de los algoritmos de busqueda.
+template <class game>
+game get_episode(Network_evaluator& Model, std::string slection_mode, BoardGame BG)
 {
-  game episode;
+    game episode;
+    std::vector<char> state;
+    std::vector<double> prob;
+    std::vector<int> valid_actions;
+    MCT_Net Black(); // Esto lo incluiré cuando haya hecho MCT_Net
+    MCT_Net White(); //...
 
-  while()
-  {
+    for (int move = 0; move < 70 && !BG.is_complete(); ++move)
+    {
+        state = BG.show_current_state();
 
-  }
+        if (move%2)
+        {
+            auto action = White.search(BG);
+            prob = White.get_probabilities_current_state();
+        }
+        else
+        {
+            auto action = Black.search(BG);
+            prob = Black.get_probabilities_current_state();
+        }
+        Black.fit_precompute_tree(action);
+        White.fit_precompute_tree(action);
 
-  return episode;
+        valid_actions = BG.get_available_sample_cells(1.0);
+        BG.make_action(action);
+
+        episode.add_game_state(prob, state, valid_actions);
+    }
+
+    episode.set_reward(BG.reward('B'));
+
+    return episode;
 }
 
-
-void generate_games(std::string path, int games , Network_evaluator& Model , std::string slection_mode, BoardGame& BG )
+void generate_games(std::string path,
+                    int games,
+                    Network_evaluator& Model,
+                    std::string slection_mode,
+                    BoardGame& BG)
 {
-  std::vector<game> episodes;
-  for(int i = 0; i < games; ++i)
-  {
-    episodes.push_back(get_episode(Model , selection_mode ,BG));
-  }
+    std::vector<game> episodes;
+    for (int i = 0; i < games; ++i)
+    {
+        episodes.push_back(get_episode(Model, selection_mode, BG));
+    }
 
-  save_games_recordings_to_json(path ,  episodes);
+    save_games_recordings_to_json(path, episodes);
 }
-
 
 template <class encoder>
-std::vector<element> get_data_games(std::string& DataPath, encoder&  Encoder_,const BoardGame& BG)
+std::vector<element> get_data_games(std::string& DataPath, encoder& Encoder_)
 {
-  std::vector<element> X;
+    std::vector<element> X;
 
-  std::vector<game> games_played = get_json_to_game_data<game>(DataPath);
+    std::vector<game> games_played = get_json_to_game_data<game>(DataPath);
 
-  for(game episode :  games_played)
-  {
-    auto encoded_states = Encoder_.Encode_episode( episode);
-    X.insert(X.end() , encoded_states.begin() , encoded_states.end());
-  }
+    for (game episode : games_played)
+    {
+        auto encoded_states = Encoder_.Encode_episode(episode);
+        X.insert(X.end(), encoded_states.begin(), encoded_states.end());
+    }
 
-  return X;
+    return X;
 }
--- Search_Algorithms/C_49/game_structure.hpp
+++ Search_Algorithms/C_49/game_structure.hpp
@@ -3,43 +3,29 @@
 
 using vertex = std::int64_t;
 
-
 struct game
 {
-    explicit game(){}
-    explicit game(
-            std::vector<std::vector<double>>& prob,
-            std::vector<std::vector<vertex>>& valid_actions_ ,
-            std::vector<std::vector<char>>& states_,
-            int black_reward_)
+    explicit game() {}
+    explicit game(std::vector<std::vector<double>>& prob,
+                  std::vector<std::vector<vertex>>& valid_actions_,
+                  std::vector<std::vector<char>>& states_,
+                  int black_reward_)
 
-            : 
-                states(states_) , 
-                probabilities(prob),
-                valid_actions(valid_actions_),
-                black_reward(black_reward_)
-            {}
+        : states(states_)
+        , probabilities(prob)
+        , valid_actions(valid_actions_)
+        , black_reward(black_reward_)
+    {}
 
-    int episode_size() const 
-    {
-        return states.size();
-    }
+    int episode_size() const { return states.size(); }
 
-    int get_reward() const 
-    {
-        return black_reward;
-    }
+    int get_reward() const { return black_reward; }
 
-    void set_reward(int value)
-    {
-        black_reward = value;
-    }
+    void set_reward(int value) { black_reward = value; }
 
-  void add_game_state(
-        const std::vector<double>& prob , 
-        const std::vector<char>&  state,
-        const std::vector<vertex>& valid_actions_
-        )
+    void add_game_state(const std::vector<double>& prob,
+                        const std::vector<char>& state,
+                        const std::vector<vertex>& valid_actions_)
     {
         states.push_back(state);
         probabilities.push_back(prob);
@@ -51,4 +37,3 @@ struct game
     std::vector<std::vector<vertex>> valid_actions;
     int black_reward;
 };
-
