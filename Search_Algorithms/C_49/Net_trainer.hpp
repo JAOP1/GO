abd8e803e68e98e708804d49b4d7fab5794dff23
Armando Ortiz <armandopenafiel12@gmail.com>
2020-06-12 09:33:12 -0500

    Torch cambios.

--- Search_Algorithms/C_49/Net_trainer.hpp
+++ Search_Algorithms/C_49/Net_trainer.hpp
@@ -1,29 +1,30 @@
 #pragma once
-#include <torch/torch.h>
 #include "UCT_Net.hpp"
+#include <torch/torch.h>
 //#include "RAVE_Net.hpp" //No he implementado nada de RAVE! D:
+#include "Data_manage.hpp"
 #include "Net_Class.hpp"
 #include "encoders.hpp"
-#include "Data_manage.hpp"
 #include "torch_utils.hpp"
-#include <string>
 #include <filesystem>
-
+#include <string>
 
 size_t dataset_size;
-
 
 // Aqui probar cosas. (Falta por hacer!)
 template <typename loss>
 loss calculate_loss(torch::Tensor& data, torch::Tensor target)
 {
     int policy_vector_leght = target.sizes()[1];
-    torch::Tensor policy_target = target.index({Slice() , Slice(None, policy_vector_leght-1)});
-    torch::Tensor value_target = target.index({Slice() , policy_vector_leght-1});
-    torch::Tensor policy_data = data.index({Slice() , Slice(None , policy_vector_leght -1)});
-    torch::Tensor value_data = data.index({Slice() , policy_vector_leght -1}); 
+    torch::Tensor policy_target = target.index(
+      {Slice(), Slice(None, policy_vector_leght - 1)});
+    torch::Tensor value_target = target.index({Slice(), policy_vector_leght - 1});
+    torch::Tensor policy_data = data.index(
+      {Slice(), Slice(None, policy_vector_leght - 1)});
+    torch::Tensor value_data = data.index({Slice(), policy_vector_leght - 1});
 
-    auto output1 = torch::nn::functional::binary_cross_entropy(policy_data,policy_target);
+    auto output1 = torch::nn::functional::binary_cross_entropy(policy_data,
+                                                               policy_target);
     auto output2 = torch::nn::functional::mse_loss(value_data, value_target);
     return output1 + output2;
 }
@@ -36,44 +37,40 @@ void train_one_epoch(Network_evaluator& model,
     auto device = nn_utils::get_device();
 
     model.train();
-    size_t batch_idx = 0;
+    //size_t batch_idx = 0;
     for (auto& batch : loader)
     {
         auto data = batch.data.to(device);
         auto targets = batch.target.to(device);
         optimizer.zero_grad();
         torch::Tensor output = model.forward(data);
+        //std::cout<<"Pasando los datos por el modelo"<<std::endl;
         torch::Tensor loss = calculate_loss<torch::Tensor>(output, targets);
+        //std::cout <<"Se ha calculado el error"<<std::endl;
         loss.backward();
+        //std::cout<<"backward terminado"<<std::endl;
         optimizer.step();
-
-        if (batch_idx++%4 == 0)
-        {
-            std::printf("\rTrain Epoch: [%5ld/%5ld] Loss: %.4f",
-                        batch_idx*batch.data.size(0),
-                        dataset_size,
-                        loss[0].item<double>());
-        }
+        //std::cout<<"optimizador"<<std::endl;
+        
     }
 }
 
-
-//Parcialmente completo.
-template<class search_type,class Encoder>
+// Parcialmente completo.
+template <class search_type, class Encoder>
 void train_model(std::string ModelPath,
                  std::string DataPath,
                  BoardGame& G,
                  int games,
                  int Batch_size,
                  int Num_epoch,
-                 nn_utils::neural_options& options_
-                 )
+                 nn_utils::neural_options& options_)
 {
+    
     Network_evaluator Model(options_);
     Network_evaluator Model_tmp(options_);
-    
-    nn_utils::load_net<Network_evaluator>(ModelPath , Model,options_);
-    nn_utils::load_net<Network_evaluator>(ModelPath, Model_tmp,options_);
+
+    nn_utils::load_net<Network_evaluator>(ModelPath, Model, options_);
+    nn_utils::load_net<Network_evaluator>(ModelPath, Model_tmp, options_);
     auto device = nn_utils::get_device();
 
     Model.to(device);
@@ -81,49 +78,55 @@ void train_model(std::string ModelPath,
     Model_tmp.eval();
 
     Encoder Encoder_(G.Board);
-    torch::optim::SGD optimizer(Model.parameters(), torch::optim::SGDOptions(0.01).momentum(0.5));
+    torch::optim::SGD optimizer(Model.parameters(),
+                                torch::optim::SGDOptions(0.01).momentum(0.5));
 
     // Condición de paro. (Falta por hacer!)
     int valor_ini = 2;
-    while (valor_ini--)
+    while (valor_ini)
     {
-        std::cout<<"Generando juegos."<<std::endl;
-        generate_games<search_type,Encoder>(/*Path_to_save = */ DataPath,
-                       /*total_records = */ games,
-                       /*Model = */ Model_tmp,
-                       /*BoardGame = */ G,
-                       /*Encoder*/ Encoder_);
+        std::cout << "Generando juegos." << std::endl;
+        generate_games<search_type, Encoder>(/*Path_to_save = */ DataPath,
+                                             /*total_records = */ games,
+                                             /*Model = */ Model_tmp,
+                                             /*BoardGame = */ G,
+                                             /*Encoder*/ Encoder_);
 
-        std::cout<<"Creando conjunto de datos."<<std::endl;
+        std::cout << "Creando conjunto de datos." << std::endl;
         auto data = get_data_games<Encoder>(/*Path_to_load=*/DataPath,
-                                   /*Encoder= */ Encoder_); // This return a
-                                                            // torch example vector.
+                                            /*Encoder= */ Encoder_); // This
+                                                                     // return a
+                                                                     // torch
+                                                                     // example
+                                                                     // vector.
         auto Dataset = GameDataSet(data).map(
           torch::data::transforms::Stack<>()); // Transform the dataset in
                                                // understable form for torch.
 
-        torch::data::samplers::RandomSampler sampler(
-          data.size()); // This say how will be getting the data.
-          
+        torch::data::samplers::RandomSampler sampler(data.size()); // This say
+                                                                   // how will
+                                                                   // be getting
+                                                                   // the data.
+
         auto DataLoader =
           torch::data::make_data_loader(Dataset, sampler, Batch_size);
-          
-        std::cout<<"Training net by "<<Num_epoch<<" epochs."<<std::endl;
+
+        std::cout << "Training net by " << Num_epoch << " epochs." << std::endl;
         for (int epoch_ = 1; epoch_ <= Num_epoch; ++epoch_)
             train_one_epoch(Model, *DataLoader, optimizer);
 
-        
-        search_type current(G , Model , Encoder_, 60 , 'B');
-        search_type last( G , Model_tmp , Encoder_ , 60 , 'W');
-        std::cout<<"Compare progress."<<std::endl;
-        if (evaluate_accuracy<search_type , search_type>(current , last, G, 30) >= .6)
+        search_type current(G, Model, Encoder_, 60, 'B');
+        search_type last(G, Model_tmp, Encoder_, 60, 'W');
+        std::cout << "Compare progress." << std::endl;
+        if (evaluate_accuracy<search_type, search_type>(current, last, G, 30) >=
+            .6)
         {
-            std::cout<<"Obtuvo exito, ha mejorado el modelo"<<std::endl;
-            nn_utils::save_net<Network_evaluator>(ModelPath , Model);
-            nn_utils::load_net<Network_evaluator>(ModelPath , Model_tmp,options_);
+            std::cout << "Obtuvo exito, ha mejorado el modelo" << std::endl;
+            nn_utils::save_net<Network_evaluator>(ModelPath, Model);
+            nn_utils::load_net<Network_evaluator>(ModelPath, Model_tmp, options_);
             Model_tmp.to(device);
             Model_tmp.eval();
+            valor_ini--;
         }
-
     }
 }
--- training_main.cpp
+++ training_main.cpp
@@ -3,39 +3,36 @@
 #include "Include/Extra/Graph.hpp"
 #include "Include/Extra/Utilities.hpp"
 #include "Include/Extra/json_manage.hpp"
-//Incluir estas lineas.
+// Incluir estas lineas.
 //#include "Search_Algorithms/C_49/RAVE_Net.hpp"
 //#include "Search_Algorithms/C_49/UCT_Net.hpp"
 //#include "Search_Algorithms/C_49/game_structure.hpp"
 //#include "Search_Algorithms/C_49/Net_Class.hpp"
 #include "Search_Algorithms/C_49/Net_trainer.hpp"
 #include "Search_Algorithms/C_49/torch_utils.hpp"
-#include <torch/torch.h>
 #include <exception>
 #include <filesystem>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <torch/torch.h>
 #include <vector>
 
-template<class T>
-std::ostream& operator<<(std::ostream& os , std::vector<T>& array)
+template <class T>
+std::ostream& operator<<(std::ostream& os, std::vector<T>& array)
 {
-  for(auto i : array)
-    os<<i<<" ";
+    for (auto i : array)
+        os << i << " ";
 
-  return os;
+    return os;
 }
-
 
 struct graph_position
 {
-  graph_position(int a, int b):x(a) , y(b){}
-  int x;
-  int y;
+    graph_position(int a, int b) : x(a), y(b) {}
+    int x;
+    int y;
 };
-
-
 
 int main(int argc, char** argv)
 {
@@ -56,8 +53,6 @@ int main(int argc, char** argv)
     app.add_option("-p", selection_mode, "type name (default = UCT): RAVE");
     CLI11_PARSE(app, argc, argv);
 
-
-
     try
     {
         std::vector<std::string> v;
@@ -65,117 +60,42 @@ int main(int argc, char** argv)
 
         std::tuple<Graph, std::vector<graph_position>> graph_ =
           get_json_to_graph_GUI<graph_position>(GraphFile);
+
         Graph G = std::get<0>(graph_);
         BoardGame BG(G);
 
         std::string directory_path = std::filesystem::current_path();
         std::string ModelPath = directory_path +
           "/../Search_Algorithms/C_49/Models/Model_" + v[0] + ".pt";
-        std::cout<<"Model saved in: "<<ModelPath<<std::endl;
 
         std::string DataPath = directory_path +
           "/../Search_Algorithms/C_49/Data/Data_" + v[0] + ".json";
 
-        std::cout<<"Data saved in: "<<DataPath<<std::endl;
+        std::cout << "Model saved in: " << ModelPath << std::endl;
+        std::cout << "Data saved in: " << DataPath << std::endl;
 
-        //TODO: Crear un archivo para todas las configuraciones de arquitectura.
-        nn_utils::neural_options options_({{3,5,3}, {5,10,3}} ,
-                          {{6210,1540},{1540,360},{360,27}});
-
-        if(selection_mode == "UCT")
-          train_model<MCTS_Net<Network_evaluator,SimpleEncoder1d >,SimpleEncoder1d>(/*Model_Path=*/ModelPath,/*Data_path=*/ DataPath,/*GameRuleSet=*/ BG, Num_games,/*Batch_size=*/ batch,/*EpochNum=*/ epoch , options_);
-        //else if(selection_mode == "RAVE")
-        //  train_model<MC_RAVE<Network_evaluator,SimpleEncoder1d>,SimpleEncoder1d>(ModelPath, DataPath, BG, Num_games, batch, epoch, options_);
+        // TODO: Crear un archivo para todas las configuraciones de arquitectura.
+        nn_utils::neural_options options_({{3, 5, 3}, {5, 10, 3}},
+                                          {{6210, 1540}, {1540, 360}, {360, 27}});
+        //torch::autograd::AnomalyMode::set_enabled(true); //NO sé como usar...
+        if (selection_mode == "UCT")
+            train_model<MCTS_Net<Network_evaluator, SimpleEncoder1d>,
+                        SimpleEncoder1d>(/*Model_Path=*/ModelPath,
+                                         /*Data_path=*/DataPath,
+                                         /*GameRuleSet=*/BG,
+                                         Num_games,
+                                         /*Batch_size=*/batch,
+                                         /*EpochNum=*/epoch,
+                                         options_);
+        // else if(selection_mode == "RAVE")
+        //  train_model<MC_RAVE<Network_evaluator,SimpleEncoder1d>,SimpleEncoder1d>(ModelPath,
+        //  DataPath, BG, Num_games, batch, epoch, options_);
     }
     catch (const std::exception& e)
     {
-        std::cout <<"Invalid graph file."<< std::endl;
-        std::cerr<<e.what();
+        std::cout << "Invalid graph file." << std::endl;
+        std::cerr << e.what();
     }
 
-
-
-
-//   //Todo esto fue para probar!! 
-//   int rows = 4;
-//   int columns = 4;
-//   Graph G = graphs::Grid(rows, columns);
-//   BoardGame BG(G);
-//   game D;
-//   SimpleEncoder2d encoder2d(G);
-//   SimpleEncoder1d encoder1d(G);
-
-//   std::vector<vertex> actions = BG.get_available_sample_cells(1.0);
-//   D.add_game_state({0.25, 0.25, 0.25, 0.25}, BG.show_current_state(), actions);
-
-//   BG.make_action(0);
-//   actions = BG.get_available_sample_cells(1.0);
-//   D.add_game_state({0, 0.333, 0.333, 0.333}, BG.show_current_state(), actions);
-
-//   BG.make_action(1);
-//   actions = BG.get_available_sample_cells(1.0);
-//   D.add_game_state({0, 0, 0.5, 0.5}, BG.show_current_state(), actions);
-
-//   D.set_reward(BG.reward('B'));
-
-//   auto vector_torch_planes_2d = encoder2d.Encode_episode<game>(D);
-//   auto vector_torch_planes_1d = encoder1d.Encode_episode<game>(D);
-
-//   std::cout<<"Encoder funciona bien!"<<std::endl;
-  
-//   //std::cout<<"Resultado con encoder 2d: "<<std::endl;
-//   //std::cout<<vector_torch_planes_2d[1].data<<std::endl;
-//   //std::cout<<vector_torch_planes_2d[1].target<<std::endl;
-  
-//   //std::cout<<"Resultado con encoder 1d:"<<std::endl;
-//   //std::cout<<vector_torch_planes_1d[1].data<<std::endl;
-//   //std::cout<<vector_torch_planes_1d[1].target<<std::endl;
-
-
-//  neural_options options_({{3,5,3}, {5,10,3}} ,
-//                          {{6210,1540},{1540,360},{360,27}});
-
-//   //Network_evaluator NN("Search_Algorithms/C_49/options_net.txt");
-//   Network_evaluator NN(options_);
-//   auto device = get_device(/*verbose=*/true);
-//   NN.to(device);
-//   NN.eval();
-
-//   std::string path = "prueba_net.pt";
-//   std::cout<<"Salvando modelo..."<<std::endl;
-//   save_net<Network_evaluator>(path, NN);
-//   std::cout<<NN<<std::endl;
-//   std::cout<<"Red neuronal creada!"<<std::endl;
-
-//   // MCTS_Net<Network_evaluator , SimpleEncoder1d> agente(BG,NN,encoder1d,323, 'B');
-//   // std::cout<<agente.search(BG)<<std::endl;
-//   // std::cout<<agente.get_probabilities_current_state()<<std::endl;
-//   // torch::Tensor input = encoder1d.Encode_data(D.states[0], D.valid_actions[0], 0,true);
-  
-//   // auto input_cud = input.to(device);
- 
-//   //  auto resultado = NN.forward(input_cud);
-//   //  std::cout<<resultado<<std::endl;
-//   //  std::cout<<resultado[0][26].item<double>()<<std::endl;
- 
-
-
-//   torch::data::samplers::RandomSampler sampler(vector_torch_planes_1d.size());
-//   auto  F = GameDataSet(vector_torch_planes_1d).map(torch::data::transforms::Stack<>());
-//   auto data_loader = torch::data::make_data_loader(F, sampler, 2);
-
-//   for (const auto& batch : *data_loader) {
-//     auto data = batch.data.to(device), targets = batch.target.to(device);
-    
-//     //std::cout<<data.sizes()<<std::endl;
-//     //std::cout<<targets.sizes()<<std::endl;
-//     auto output = NN.forward(data);
-//     std::cout<<calculate_loss<torch::Tensor>(output , targets)<<std::endl;
-//     std::cout<<output.sizes()<<std::endl;
-//     std::cout<<targets.sizes()<<std::endl;
-//     std::cout<<"----"<<std::endl;
-//   }
-
-
-  return 0;
+    return 0;
 }
